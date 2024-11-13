b215dbd07f3b3a516180fbf18a6503f3e34c2469
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-11-13 07:02:53 +0530

    finally testing completed

---
+++ client/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+export default {
+    server: {
+      host: '0.0.0.0',  // This allows external access (e.g., from your host machine)
+      port: 5173         // This will be the port exposed by Docker
+    }
+  };
+  
